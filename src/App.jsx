01fb07e680665fa30fe51ab2a723922d205d910a
Zahra Karandish <zkarandish@gmail.com>
2024-02-21 12:39:11 -0500

    reduce redundancy

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { useState } from "react";
 
+function derivedActivePlayer(gameTurns){
+  let currentPlayer = 'X';
+
+  if(gameTurns.length>0 && gameTurns[0].player === 'X'){
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 
 function App() {
   const[gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+ // const [activePlayer, setActivePlayer] = useState('X');
+
+ const activePlayer= derivedActivePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, colIndex){
-    setActivePlayer((curActivePlayer)=>  (curActivePlayer === 'X' ? 'O' : 'X'));
+   // setActivePlayer((curActivePlayer)=>  (curActivePlayer === 'X' ? 'O' : 'X'));
 
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if(prevTurns.length>0 && prevTurns[0].player === 'X'){
-        currentPlayer = 'O';
-      }
+      const currentPlayer = derivedActivePlayer(prevTurns);
       const updatedTurns = [
         {square:{row:rowIndex, col: colIndex},player:currentPlayer},
         ...prevTurns,
