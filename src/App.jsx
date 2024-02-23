bfcf933707c4b5fcedc07918d1bf200d767b4620
Zahra Karandish <zkarandish@gmail.com>
2024-02-23 09:16:18 -0500

    final touch ups

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import { useState } from "react";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 import GameOver from "./components/GameOver";
 
-
-const initialGameBoard = [
+const PLAYERS ={
+  'X': 'Player 1',
+  'O': 'Player 2'
+};
+const INITAIL_GAME_BOARD = [
   [null,null,null],
   [null,null,null],
   [null,null,null],
@@ -21,27 +24,21 @@ function derivedActivePlayer(gameTurns){
   return currentPlayer;
 }
 
+function deriveGameBoard(gameTurns){
+  let gameBoard = [...INITAIL_GAME_BOARD.map(array => [...array])];
 
-function App() {
-  const [players, setPlayers] = useState({
-    'X': 'Player 1',
-    'O': 'Player 2',
-  });
-  const[gameTurns, setGameTurns] = useState([]);
- // const [activePlayer, setActivePlayer] = useState('X');
-
- const activePlayer= derivedActivePlayer(gameTurns);
-
- let gameBoard = [...initialGameBoard.map(array => [...array])];
-
-for(const turn of gameTurns){
-const { square, player}= turn;
-const {row, col} = square;
-
-gameBoard[row][col] = player;
+  for(const turn of gameTurns){
+  const { square, player}= turn;
+  const {row, col} = square;
+  
+  gameBoard[row][col] = player;
+  }
+  return gameBoard;
 }
 
-let winner;
+
+function deriveWinner(gameBoard, players){
+  let winner;
 
  for (const combination of WINNING_COMBINATIONS){
   const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
@@ -53,6 +50,21 @@ let winner;
 
   } 
  }
+ return winner;
+}
+
+function App() {
+  const [players, setPlayers] = useState(PLAYERS)
+
+  const[gameTurns, setGameTurns] = useState([]);
+ // const [activePlayer, setActivePlayer] = useState('X');
+
+ const activePlayer= derivedActivePlayer(gameTurns);
+
+const gameBoard = deriveGameBoard(gameTurns);
+
+const winner = deriveWinner(gameBoard,players);
+
  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex){
@@ -87,13 +99,13 @@ let winner;
     <div id="game-container">
       <ol id="players" className="highlight-player">
         <Player 
-        initialName="player1" 
+        initialName={PLAYERS.X}
         symbol="X" 
         isActive={activePlayer === 'X'}
         onChangeName= {handlePlayerNameChange}
         />
         <Player 
-        initialName="player2"
+        initialName={PLAYERS.O}
          symbol="O" 
          isActive={activePlayer === 'O'}
          onChangeName= {handlePlayerNameChange}
