88f3b93f71f87686f86fc8fc4a518c85cc88fa98
Zahra Karandish <zkarandish@gmail.com>
2024-02-23 08:00:21 -0500

    the draw is manipulated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { useState } from "react";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver";
+
+
+const initialGameBoard = [
+  [null,null,null],
+  [null,null,null],
+  [null,null,null],
+];
 
 function derivedActivePlayer(gameTurns){
   let currentPlayer = 'X';
@@ -18,6 +27,29 @@ function App() {
  // const [activePlayer, setActivePlayer] = useState('X');
 
  const activePlayer= derivedActivePlayer(gameTurns);
+
+ let gameBoard = initialGameBoard;
+
+for(const turn of gameTurns){
+const { square, player}= turn;
+const {row, col} = square;
+
+gameBoard[row][col] = player;
+}
+
+let winner;
+
+ for (const combination of WINNING_COMBINATIONS){
+  const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+  const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+  const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+  if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
+    winner = firstSquareSymbol;
+
+  } 
+ }
+ const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIndex, colIndex){
    // setActivePlayer((curActivePlayer)=>  (curActivePlayer === 'X' ? 'O' : 'X'));
@@ -41,9 +73,10 @@ function App() {
         <Player initialName="player1" symbol="X" isActive={activePlayer === 'X'}/>
         <Player initialName="player2" symbol="O" isActive={activePlayer === 'O'}/>
       </ol>
+      {(winner || hasDraw) && <GameOver winner={winner}/>}
       <GameBoard 
       onSelectSquare={handleSelectSquare} 
-      turns= {gameTurns}
+      board= {gameBoard}
       />
       </div>
       <Log turns={gameTurns}/>
